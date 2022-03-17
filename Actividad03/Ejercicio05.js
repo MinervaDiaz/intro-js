d1b5cd0f18489885cd478ecb22f9f2eb2f12ff1f
MinervaD <minervadiago@gmail.com>
2022-03-17 17:39:06 -0600

    Ejercicios if/else, disminución de código en ejercicio05

--- Actividad03/Ejercicio05.js
+++ Actividad03/Ejercicio05.js
@@ -2,46 +2,26 @@ var numeroUno = Number(prompt("Ingrese el número 1"))
 var numeroDos = Number(prompt("Ingrese el número 2"))
 var numeroTres = Number(prompt("Ingrese el número 3"))
 
-//DETERMINAR CUÁL DE LOS TRES NÚMEROS ES MAYOR
+//DETERMINAR CUÁL DE LOS TRES NÚMEROS ES MAYOR Y SI LOS DOS RESTANTES SON IGUALES
 if(numeroUno > numeroDos && numeroUno > numeroTres){
-    console.log("El numero", numeroUno, "es mayor")
+    console.log("El número", numeroUno, "es mayor")
+    if(numeroDos == numeroTres){
+        console.log("El número 2:",numeroDos, "y el número 3:",numeroTres," son iguales")
+    }
 }
 else if(numeroDos > numeroUno && numeroDos > numeroTres){
-    console.log("El numero", numeroDos, "es mayor")
+    console.log("El número", numeroDos, "es mayor")
+    if(numeroUno == numeroTres){
+        console.log("El número 1:",numeroUno, "y el número 3:",numeroTres," son iguales")
+    }
 }
 else if(numeroTres > numeroDos && numeroTres > numeroUno){
-    console.log("El numero", numeroTres, "es mayor")
-}
-//DETERMINAR SI HAY DOS NUMEROS IGUALES SIENDO LOS MAYORES
-else if(numeroUno>numeroDos && numeroUno == numeroTres){
-    console.log("El numero 1: ",numeroUno," y el numero 3: ",numeroTres," son iguales y son los mayores")
-}
-else if(numeroUno>numeroTres && numeroUno == numeroDos){
-    console.log("El numero 1: ",numeroUno, " y el numero 2: ",numeroDos," son iguales y son los mayores")
-}
-else if(numeroDos>numeroTres && numeroDos == numeroUno){
-    console.log("El numero 2: ",numeroDos," y el numero 1: ",numeroUno," son iguales y son los mayores")
-}
-else if(numeroDos>numeroUno && numeroDos == numeroTres){
-    console.log("El numero 2: ",numeroDos," y el numero 3: ",numeroTres," son iguales y son los mayores")
-}
-else if(numeroTres>numeroUno && numeroTres == numeroDos){
-    console.log("El numero 3: ",numeroTres," y el numero 2: ",numeroDos," son iguales y son los mayores")
-}
-else if(numeroTres>numeroDos && numeroTres == numeroUno){
-    console.log("El numero 3: ",numeroTres," y el numero 2: ",numeroDos," son iguales y son los mayores")
-}
-//DETERMINAR SI HAY UN NÚMERO MAYOR TENIENDO IGUALES A LOS OTROS DOS
-else if(numeroUno > numeroDos && numeroUno > numeroTres && numeroDos==numeroTres){
-    console.log("El numero", numeroUno, "es mayor y los numeros 2: ",numeroDos," y 3: ",numeroTres," son iguales")
-}
-else if(numeroDos > numeroUno && numeroDos > numeroTres && numeroUno==numeroTres){
-    console.log("El numero", numeroDos, "es mayor y los numeros 1: ",numeroUno," y 3: ",numeroTres," son iguales")
-}
-else if(numeroTres > numeroDos && numeroTres > numeroUno){
-    console.log("El numero", numeroTres, "es mayor y los numeros 2: ",numeroDos," y 1: ",numeroUno," son iguales")
+    console.log("El número", numeroTres, "es mayor")
+    if(numeroUno == numeroDos){
+        console.log("El número 1:",numeroUno, "y el número 2:",numeroDos," son iguales")
+    }
 }
 //DETERMINAR SI LOS TRES NÚMEROS SON IGUALES
-else{
-    console.log("Los tres numeros son iguales. Numero 1: ",numeroUno,"Numero 2: ",numeroDos," Numero 3: ",numeroTres)
+if (numeroUno == numeroDos && numeroUno == numeroTres){
+    console.log("Los tres números son iguales. Número 1:",numeroUno,"Número 2:",numeroDos," Número 3:",numeroTres)
 }
